c5458d7c738bceecc9579b5ea3ce3b81e53c5a55
Fayyaz Alam <fayazalam.98@gmail.com>
2021-06-21 00:29:16 -0700

    call timer function added with condition

--- js/app.js
+++ js/app.js
@@ -3,3 +3,21 @@ var sec = 0;
 var miliSec = 0;
 var timer;
 
+function callTimer() {
+    miliSec++;
+    if (miliSec < 100) {
+        if (miliSec === 99) {
+            miliSec = 0;
+            sec++;
+            if (sec === 60)  {
+                sec = 0;
+                min++;
+            }
+        }
+    }
+    else{
+        miliSec = 0;
+    }
+    document.getElementById("timer").innerHTML = min + ":" + sec + ":" + miliSec;
+}
+
